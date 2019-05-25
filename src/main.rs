048196990987ca535f9afdd3fd1696e4dd2d07c2
thk2b <theokobb@gmail.com>
2019-05-25 02:10:28 -0700

    recieve packets and increase ttl

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,28 @@ extern crate pnet;
 
 use std::error::Error;
 use std::io;
+use std::time::Instant;
 use std::net::{IpAddr, Ipv4Addr};
+use std::slice;
+use std::mem;
 use pnet::{
     util::checksum,
     packet::{
+        Packet,
         MutablePacket,
         icmp::{
             self,
+            IcmpTypes,
+            Icmp,
+            IcmpPacket,
             echo_request::{
                 MutableEchoRequestPacket,
                 EchoRequestPacket,
             },
-            echo_reply::EchoReplyPacket
+            echo_reply::{
+                EchoReplyPacket,
+                EchoReply,
+            }
         },
         ip::IpNextHeaderProtocols,
     },
@@ -28,7 +38,7 @@ use pnet::{
     },
 };
 
-static DEFAULT_TTL: u16 = 64;
+static DEFAULT_TTL: u8 = 64;
 
 // struct EchoPacket { }
 
@@ -46,23 +56,21 @@ static DEFAULT_TTL: u16 = 64;
 
 // }
 
-fn new_echo_packet(buf: &mut [u8], ttl: u16) -> MutableEchoRequestPacket {
+fn new_echo_packet(buf: &mut [u8], ttl: u8) -> MutableEchoRequestPacket {
     let mut p = MutableEchoRequestPacket::new(buf).expect("echo packet");
     p.set_icmp_type(icmp::IcmpTypes::EchoRequest);
-    p.set_sequence_number(ttl);
+    p.set_sequence_number(ttl.into());
     p.set_identifier(1); // TODO
-    p.set_payload(&[1u8; 16-8]); //TODO
+    let now = Instant::now();
+    p.set_payload(&[0u8; 16-8]);
     let cksum = checksum(&p.packet_mut(), 1);
     p.set_checksum(cksum);
     p
 }
 
-fn echo_packets_match(req: &EchoRequestPacket, res: &EchoReplyPacket) -> bool {
-    true //TODO
-}
-
-fn echo_packets_time_diff(req: &EchoRequestPacket, res: &EchoReplyPacket) -> f32 {
-    0.123 // TODO
+fn time_diff(sent_at: Instant) -> f32 {
+    let diff = Instant::now().duration_since(sent_at);
+    diff.as_micros() as f32 / 1000.0
 }
 
 struct Pinger<'a> {
@@ -75,11 +83,17 @@ impl<'a> Pinger<'a> {
         Pinger { tx, rx }
     }
 
-    fn ping(&mut self, dst: &Ipv4Addr, buf: &mut [u8], ttl: u16) -> Result<f32, io::Error> {
-        let packet = new_echo_packet(buf, ttl);
-        //set ttl on tx.socket
+    fn ping(&mut self, dst: &Ipv4Addr, ttl: u8) -> Result<f32, io::Error> {
+        let mut req_buf = [0u8; 64];
+        let mut res_buf = [0u8; 64];
+
+        let packet = new_echo_packet(&mut req_buf, ttl);
+        let packet = packet.to_immutable();
+        let id = packet.get_identifier();
+        self.tx.set_ttl(ttl).expect("set ttl");
+        let sent_at = Instant::now();
         match self.tx.send_to(packet, IpAddr::V4(*dst)) {
-            Ok(sent) => println!("sent {:}", sent),
+            Ok(sent) => println!("sent {:} bytes with ttl={:}", sent, ttl),
             Err(e) => {
                 println!("error sending {:}", e);
                 return Err(e)
@@ -87,11 +101,19 @@ impl<'a> Pinger<'a> {
         };
         loop { // loop until timeout or we recieved the packet
             match self.rx.next() {
-                Ok(res) => {
-                    println!("recieved {:?}", &res);
-                    // if echo_packets_match(&packet, &res) {
-                        // return Ok(echo_packets_time_diff(&packet, &res))//res))
-                    // }
+                Ok((res, addr)) => {
+                    println!("recieved {:?} from {:}", &res, addr);
+                    match res.get_icmp_type() {
+                        IcmpTypes::TimeExceeded => (),
+                        IcmpTypes::EchoReply => {
+                            let res = EchoReplyPacket::new(res.packet()).expect("echo packet");
+                            if res.get_identifier() != id {// TODO: validate cksum
+                                continue
+                            }
+                        }
+                        _ => continue
+                    }
+                    return Ok(time_diff(sent_at))
                 },
                 Err(e) => {
                     println!("error recieving {:}", e);
@@ -104,14 +126,14 @@ impl<'a> Pinger<'a> {
 
 fn traceroute(dst: &Ipv4Addr) -> Result<(), io::Error> {
     let protocol = Layer4(Ipv4(IpNextHeaderProtocols::Icmp));
-    let (mut tx, mut rx) = transport_channel(1024, protocol)?;//TODO: timeout
-    let mut rx = icmp_packet_iter(&mut rx);
+    let (tx, mut rx) = transport_channel(1024, protocol)?; // TODO: timeout
+    let rx = icmp_packet_iter(&mut rx);
     let mut pinger = Pinger::new(tx, rx);
-    let mut buf = [0u8; 64];
 
-    for ttl in 1..DEFAULT_TTL { // do this 3 times
+    // for ttl in 1..DEFAULT_TTL { // do this 3 times
+    for ttl in 1..65 { // do this 3 times
         for _ in 0..3 {
-            match pinger.ping(dst, &mut buf[..], ttl) {
+            match pinger.ping(dst, ttl) {
                 Ok(time) => print!("{:} ms", time),
                 Err(e) => println!("error {:}", e),
             }
